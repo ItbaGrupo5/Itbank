48cd5c09749c18556201948c16bb9ca6e240598c
Juan Chianetta <juanchianetta@hotmail.com>
2022-06-19 21:35:40 -0300

    merge a la pagina principal

--- Animacion/script.js
+++ Animacion/script.js
@@ -5,7 +5,7 @@ const loader2 = document.querySelector(".logocarga")
 loader2.style.opacity=0;
 const loader = document.querySelector(".load")
 loader.style.opacity=0;
-},2000);})
+},3000);})
 
 function pagina(){
     location.href="../home.html"

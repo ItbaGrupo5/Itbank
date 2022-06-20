9a4db3c999497d89f53d21986630359eb34b5058
GeronimoGS <105251098+GeronimoGS@users.noreply.github.com>
2022-06-19 22:44:53 -0300

    Update script.js

--- Animacion/script.js
+++ Animacion/script.js
@@ -5,12 +5,12 @@ const loader2 = document.querySelector(".logocarga")
 loader2.style.opacity=0;
 const loader = document.querySelector(".load")
 loader.style.opacity=0;
-},3000);})
+},30000);})
 
 function pagina(){
     location.href="home.html"
 }
-setTimeout(pagina(),{},30000)
+setTimeout(pagina(),{},5000)
 
 
 
